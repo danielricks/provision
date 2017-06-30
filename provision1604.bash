aadf544ac6711e7cbf373a62e06e9b526e9ba8d7
Daniel Ricks <danielricks@users.noreply.github.com>
2017-06-30 11:59:22 -0600

    Removed python graph tool

--- provision1604.bash
+++ provision1604.bash
@@ -87,33 +87,6 @@ sudo apt-install -y clamav
 sudo apt-install -y clamtk
 }
 
-pythongraphtool() {
-sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
-sudo apt-get update
-sudo apt-get -y upgrade
-sudo apt-get -y install expat
-sudo apt-get -y install libsparsehash-dev
-sudo apt-get -y install gtk+3
-sudo apt-get -y install libboost-all-dev
-sudo apt-get -y install graphviz
-sudo apt-get -y install build-essential
-sudo apt-get -y install libcairo2-dev
-sudo apt-get -y install python-pip
-sudo apt-get -y install python-dev
-sudo apt-get -y install python-matplotlib
-sudo apt-get -y install gfortran libopenblas-dev liblapack-dev
-sudo apt-get -y install libcgal-dev
-sudo apt-get -y install python-numpy
-sudo apt-get -y install python2.7-config
-sudo apt-get -y install python-cairo
-sudo apt-get -y install python-scipy
-sudo apt-key adv --keyserver pgp.skewed.de --recv-key 98507F25
-echo 'deb http://downloads.skewed.de/apt/trusty trusty universe' | sudo tee -a  /etc/apt/sources.list
-echo 'deb-src http://downloads.skewed.de/apt/trusty trusty universe' | sudo tee -a  /etc/apt/sources.list
-sudo apt-get update
-sudo apt-get -y --force-yes install python-graph-tool
-}
-
 pdftk() {
 sudo apt-get -y install pdftk
 }
